cc0a9e05b6aec214c9a32159007b1ac625aa536a
BogomolSergo <breinkiller650@gmail.com>
2017-03-13 21:14:17 +0200

    PracticWork (changed)

--- Document.rb
+++ Document.rb
@@ -1,27 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
 class Document
-	@@data = [[],[]]
 	def parse (path)
-		doc = Nokogiri::HTML(open(path))
+		@doc = Nokogiri::HTML(open(path))
 		puts 'Search for nodes by css'
-		doc.css('a').each { |link|	@@data[0] << link.content }
-		doc.css('h2').each { |header|	@@data[1] << header.content }
 	end
 end
 
 class Link < Document
 	def parse(path)
 		super
-		puts "Links: #{@@data[0]}"
+		@doc.css('a').each { |link|	puts "Link: #{link.content}" }
 	end
 end
 
 class Header < Document
 	def parse(path)
 		super
-		puts "Headers: #{@@data[1]}"
+		@doc.css('h2').each { |header|	puts "Header: #{header.content}" }
 	end
 end
 
 Link.new.parse('http://www.nokogiri.org/tutorials/installing_nokogiri.html')
+Header.new.parse('http://www.nokogiri.org/tutorials/installing_nokogiri.html')
