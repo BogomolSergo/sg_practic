1019c418054da48afe31def23354c1d7454b8677
BogomolSergo <breinkiller650@gmail.com>
2017-03-14 13:40:06 +0200

    PracticWork (changed)

--- practic_lesson2.rb
+++ practic_lesson2.rb
@@ -1,56 +1,46 @@
 module Notification
 	require 'time'
+	EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	SMS = /\+380\d{9}/
 	def self.included(base)
 		base.extend(ClassMethods)
 	end
 
 	module ClassMethods
 		def log
-			if itself.name == 'Email'
-				File.open('email.log', 'r') { |f| puts f.readlines }
-				else File.open('sms.log', 'r') { |f| puts f.readlines }
-			end
+			File.open(((self.name == 'Email') ? 'email.log' : 'sms.log'), 'r') { |f| puts f.readlines }
 		end
 	end
 
 	def add_to_log(recipient)
-		if itself.class.name == 'Email'
-			File.open('email.log', 'a') { |f| f.write "Invalid email: #{recipient} was input. Error time: #{Time.now}\n" }
-		else
-			File.open('sms.log', 'a') { |f| f.write "Invalid phone number: #{recipient}. Use only mobile. Error time: #{Time.now}\n" }
-		end
+		File.open(((self.class.name == 'Email') ? 'email.log' : 'sms.log'), 'a') { |f| f.write "Invalid #{self.class.name} receiver: #{recipient} was input. Error time: #{Time.now}\n" }
 	end
 
 	def send_message(recipient)
-		yield(recipient) if block_given?
+		if ((recipient =~ EMAIL) && self.class.name == 'Email') || ((recipient =~ SMS) && self.class.name == 'Sms')
+			yield(recipient) if block_given?
+		else
+			add_to_log(recipient)
+			raise TypeError, (self.class.name == 'Email') ? 'Invalid email =)' : 'Invalid phone number! Use only mobile.'
+		end
 	end
 end
 
 class Email
 	include Notification
 	def initialize(email)
-		if email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-			send_message(email) { |recipient| puts "Sending email to #{recipient}" }
-		else
-			add_to_log(email)
-			raise TypeError, 'Invalid email =)'
-		end
+		send_message(email) { |recipient| puts "Sending email to #{recipient}" }
 	end
 end
 
 class Sms
 	include Notification
 	def initialize(number)
-		if number =~ /\+380\d{9}/
-			send_message(number) { |recipient| puts "Sending SMS to #{recipient}" }
-		else
-			add_to_log(number)
-			raise TypeError, 'Invalid phone number! Use only mobile.'
-		end
+		send_message(number) { |recipient| puts "Sending SMS to #{recipient}" }
 	end
 end
 
-#Email.new('breinkiller650gmail.com') # Fine
-#Sms.new('380930851354') # Dont
+#Email.new('breinkiller650@gmail.com') # Fine
+Sms.new('380930851354') # Dont
 #Sms.log # Show log
-Email.log
+#Email.log
