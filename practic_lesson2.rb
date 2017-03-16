c34e38faa84c6d1e7040e7e6e83a2bebbcb20587
Serhii Bohomol <breinkiller650@gmail.com>
2017-03-16 12:35:11 +0200

    RubyMine (tabs convert tp spaces) --> not found)

--- practic_lesson2.rb
+++ practic_lesson2.rb
@@ -1,73 +1,73 @@
 module Service
-	class Deliver
-		require 'gmail'
-		def sms(recipient)
-			puts "Sending sms to: #{recipient} by Deliver class method"
-		end
+  class Deliver
+  require 'gmail'
+    def sms(recipient)
+      puts "Sending sms to: #{recipient} by Deliver class method"
+    end
 
-		def email(recipient)
-			puts "Sending email to: #{recipient} by Deliver class method"
-			gmail = Gmail.connect(recipient, 'password') #password unreal =)
-			gmail.deliver do
-				to 'sergey-bogomol-1994@yandex.ru'
-				subject 'Message with Ruby mail.'
-				text_part do
-					body 'This message send with Gmail service from my ruby application.'
-				end
-			end
-			gmail.logout
-		end
+    def email(recipient)
+      puts "Sending email to: #{recipient} by Deliver class method"
+      gmail = Gmail.connect(recipient, 'password') #password unreal =)
+      gmail.deliver do
+	to 'sergey-bogomol-1994@yandex.ru'
+	subject 'Message with Ruby mail.'
+	text_part do
+	  body 'This message send with Gmail service from my ruby application.'
 	end
+      end
+      gmail.logout
+    end
+  end
 end
 
 module Notification
-	require 'time'
+require 'time'
 
-	def self.included(base)
-		base.extend(ClassMethods)
-	end
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
 
-	module ClassMethods
-		def log
-			File.open("#{name.downcase}.log", 'r') { |f| puts f.readlines }
-		end
-	end
+  module ClassMethods
+    def log
+      File.open("#{name.downcase}.log", 'r') { |f| puts f.readlines }
+    end
+  end
 
-	def add_to_log(recipient)
-		File.open("#{self.class.name.downcase}.log", 'a') { |f| f.write "
-		Invalid #{self.class.name} receiver: #{recipient} was input. Error time: #{Time.now}\n" }
-	end
+  def add_to_log(recipient)
+    File.open("#{self.class.name.downcase}.log", 'a') { |f| f.write "
+    Invalid #{self.class.name} receiver: #{recipient} was input. Error time: #{Time.now}\n" }
+  end
 
-	def send_message(recipient)
-		Service::Deliver.new.send(self.class.name.downcase, recipient)
-		yield(recipient) if block_given? # Our block-parameter use.
-	end
+  def send_message(recipient)
+    Service::Deliver.new.send(self.class.name.downcase, recipient)
+    yield(recipient) if block_given? # Our block-parameter use.
+  end
 end
 
 class Email
-	include Notification
+include Notification
 
-	def send_message(email)
-		if email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-			super(email) { |recipient| puts "Sending email to #{recipient} by block-param." }
-		else
-			add_to_log(email)
-			raise TypeError, 'Invalid email =)'
-		end
-	end
+  def send_message(email)
+    if email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      super(email) { |recipient| puts "Sending email to #{recipient} by block-param." }
+    else
+      add_to_log(email)
+      raise TypeError, 'Invalid email =)'
+    end
+  end
 end
 
 class Sms
-	include Notification
+include Notification
 
-	def send_message(number)
-		if number =~ /\+380\d{9}/
-			super(number) { |recipient| puts "Sending SMS to #{recipient} by block-param." }
-		else
-			add_to_log(number)
-			raise TypeError, 'Invalid phone number! Use only mobile.'
-		end
-	end
+  def send_message(number)
+    if number =~ /\+380\d{9}/
+      super(number) { |recipient| puts "Sending SMS to #{recipient} by block-param." }
+    else
+      add_to_log(number)
+      raise TypeError, 'Invalid phone number! Use only mobile.'
+    end
+  end
 end
 
 Email.new.send_message('breinkiller650@gmail.com') # Fine
